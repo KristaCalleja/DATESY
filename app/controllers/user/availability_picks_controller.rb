1c8d0e4631e63a1ce9ec3b67359ea835340dc470
luis frade <luis.carvalho.frade@gmail.com>
2019-12-10 16:02:10 -0300

    Added availability_picks (controller, views and routes)

---
+++ app/controllers/user/availability_picks_controller.rb
@@ -0,0 +1,12 @@
+class User::AvailabilityPicksController < ApplicationController
+  def new
+  end
+
+  def create
+    raise
+
+    # current_user.availabilities.thursday.update()
+    # current_user.availabilities.friday.update()
+    # current_user.availabilities.saturday.update()
+  end
+end
