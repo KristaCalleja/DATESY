5e8e036e49bda380383659a0f7ff729f56e5ef87
Kamrin Klauschie <kamrin.klauschie@gmail.com>
2019-12-10 20:20:56 -0300

    Corrected Matchmaker match button and reject button behavior

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,7 +2,7 @@ import "bootstrap";
 import "packs/match_button_with_modal";
 
 import { initMatchForms } from "../components/match_form";
-import { initWaves } from "../components/waves";
+// import { initWaves } from "../components/waves";
 
 initMatchForms();
-initWaves();
+// initWaves();
