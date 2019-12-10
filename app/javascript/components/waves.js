db901b62153c41767bb176eee210bee119f10594
Krista Calleja <kristacm@gmail.com>
2019-12-10 15:59:11 -0300

    Code for wave js needs review

---
+++ app/javascript/components/waves.js
@@ -0,0 +1,10 @@
+import Waves from "node-waves";
+
+const initWaves = () => {
+  document.addEventListener('DOMContentLoaded', () => {
+    Waves.attach('.large_button');
+    Waves.init();
+  };
+};
+
+export { initWaves };
