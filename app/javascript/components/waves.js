fe27244aed25bfabda0111e23b86e485a0fbe5e7
Krista Calleja <kristacm@gmail.com>
2019-12-11 09:59:43 -0300

    Fixed broken waves js. Now fully functional

--- app/javascript/components/waves.js
+++ app/javascript/components/waves.js
@@ -1,10 +1,11 @@
+import "../../../node_modules/node-waves/dist/waves.min.css"
 import Waves from "node-waves";
 
 const initWaves = () => {
   document.addEventListener('DOMContentLoaded', () => {
     Waves.attach('.large_button');
     Waves.init();
-  };
+  });
 };
 
 export { initWaves };
