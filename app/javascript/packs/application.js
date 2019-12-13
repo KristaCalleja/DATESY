1a2928d6fcd0ffa3d2303500f8eb6a95ea969772
bruna romeiro <bruna_romeiro@yahoo.com.br>
2019-12-13 10:38:58 -0300

    Remove underline and add wave js to large_btn

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,11 +4,11 @@ import "packs/match_button_with_modal";
 import { initMatchForms } from "../components/match_form";
 import { initFriendsCarousel } from "../components/friends-carousel";
 import { initCopyButton } from "../components/copy_button";
-// import { initWaves } from "../components/waves";
+import { initWaves } from "../components/waves";
 
 initMatchForms();
 initFriendsCarousel();
 initCopyButton();
-// initWaves();
+initWaves();
 
 
